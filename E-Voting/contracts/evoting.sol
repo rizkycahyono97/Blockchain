be125f2db92cc93a8ea5d9e812c9807da3cdf107
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-01-20 21:10:57 +0700

    completed

--- E-Voting/contracts/evoting.sol
+++ E-Voting/contracts/evoting.sol
@@ -6,11 +6,13 @@ pragma solidity >=0.8.2 <0.9.0;
 */
 contract evoting {
     address public owner; //owner itu yang membuat/ketua voting tersebut
-    bool public sudahBisaVoting;
+    bool public isSudahBisaVoting;
+    bool public isVotingSelesai;
 
     constructor() {
         owner = msg.sender;
-        sudahBisaVoting  = false;
+        isSudahBisaVoting  = false;
+        isVotingSelesai = false;
     }
 
     struct Kandidat {
@@ -19,7 +21,9 @@ contract evoting {
     }
 
     Kandidat[] public  kandidat;    //variable untuk struct kandidat
-    mapping(address => bool) public kandidatTerdaftar;
+
+    mapping(address => bool) public isKandidatTerdaftar;
+    mapping (address => bool) public isUserSudahMemilih;
 
     /*
         - untuk mendaftarkan kandidat
@@ -29,9 +33,9 @@ contract evoting {
     function daftarkanKandidat(address _addr) public {
         require(msg.sender == owner, "Anda bukan owner, hanya owner yang bisa mendaftarkan kandidat");
         require(_addr != owner, "Owner tidak boleh mendaftarkan dirinya sendiri");
-        require(!kandidatTerdaftar[_addr], "Kandidat sudah mendaftar");
+        require(!isKandidatTerdaftar[_addr], "Kandidat sudah mendaftar");
         kandidat.push(Kandidat(_addr, 0));  //push ke struct kandidat
-        kandidatTerdaftar[_addr] = true;
+        isKandidatTerdaftar[_addr] = true;
     }
 
     /*
@@ -46,14 +50,41 @@ contract evoting {
     */
     function mulaiVoting(bool _voting) public {
         require(msg.sender == owner, "Anda bukan owner, hanya owner yang bisa mendaftarkan kandidat");
-        sudahBisaVoting = _voting;
+        isSudahBisaVoting = _voting;
     }
 
     /*
         - untuk memilih kandidat
     */
     function voting(uint _urutanKandidat) public {
-        require(sudahBisaVoting == true, "Voting belum dimulai");
+        require(isSudahBisaVoting == true, "Voting belum dimulai");
+        require(_urutanKandidat < kandidat.length, "Kandidat tidak ditemukan");
+        require(!isUserSudahMemilih[msg.sender], "User sudah memilih");
         kandidat[_urutanKandidat].jumlahSuara++;
+        isUserSudahMemilih[msg.sender] = true;   //jadika true jika sudah memilih
     } 
+
+    function votingSelesai() public {
+        require(msg.sender == owner, "Anda bukan owner");
+        isVotingSelesai = true;
+        isSudahBisaVoting = false;
+    }
+
+    function pemenang() public  view returns (address, uint) {
+        // require(isSudahBisaVoting, "Voting belum dimulai");
+        require(kandidat.length >= 2, "Kandidat tidak ada");    //minimal ada 2 kandidat yang daftar
+        require(isVotingSelesai, "Voting belum selesai");
+
+        uint suaraTertinggi = 0;
+        uint indexPemenang = 0;
+
+        for (uint i = 0; i < kandidat.length; i++) {
+            if (kandidat[i].jumlahSuara > suaraTertinggi) {
+                suaraTertinggi = kandidat[i].jumlahSuara;
+                indexPemenang = i;
+            }
+        }
+
+        return (kandidat[indexPemenang]._addr, kandidat[indexPemenang].jumlahSuara);
+    }
 }
