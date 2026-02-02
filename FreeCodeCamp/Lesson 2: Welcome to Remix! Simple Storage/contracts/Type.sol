51a5fb2b2db1893353c9469f03243d851115c76d
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-02-02 16:39:08 +0700

    add FreeCodeCamp & Sol Type

---
+++ FreeCodeCamp/Lesson 2: Welcome to Remix! Simple Storage/contracts/Type.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Type{
+    // solidity type: boolean, int, uint, address, bytes
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNUmber = 80;
+    string favoriteNumberInText = "Red";
+    int256 favoriteInt = -1;
+    address myAddress = 0x7746085DD07196b6efc1F095BeE6FFCE95DFcb9f;
+    bytes32 favoriteByte = "cat";   
+}
