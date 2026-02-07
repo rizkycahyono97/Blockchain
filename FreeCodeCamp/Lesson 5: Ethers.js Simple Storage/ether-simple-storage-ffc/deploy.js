d09e8f25cbec87f26daaa548a4271635ba9e527f
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-02-07 22:27:27 +0700

    add sendTransaction()

--- FreeCodeCamp/Lesson 5: Ethers.js Simple Storage/ether-simple-storage-ffc/deploy.js
+++ FreeCodeCamp/Lesson 5: Ethers.js Simple Storage/ether-simple-storage-ffc/deploy.js
@@ -22,9 +22,25 @@ async function deploy() {
 
     const contract = await factory.deploy();
     await contract.waitForDeployment();
+    const deploymentReceipt = await contract.deploymentTransaction().wait(1); // wait for 1 confirmation
 
     console.log(`Berhasil! Alamat: ${contract.target}`);
-    console.log(contract);
+    console.log(deploymentReceipt);
+
+    // just for send transaction
+    // console.log('Mengirim transaksi...');
+    // const nonce = await wallet.getTransactionCount();
+    // const tx = {
+    //   nonce: nonce,
+    //   gasPrice: 200000000,
+    //   gasLimit: 1000000,
+    //   to: null,
+    //   value: 0,
+    //   data: '0xb85824f1294ee69d09df6574c7a60820f958c7e8634f4892bebeeb23c63436b7'
+    // };
+    // const sendTxResponse = await wallet.sendTransaction(tx);
+    // const transactionReceipt = await sendTxResponse.wait(1);
+    // console.log('transactionReceipt:', transactionReceipt);
   } catch (e) {
     console.error('Gagal karena:', e);
   }
