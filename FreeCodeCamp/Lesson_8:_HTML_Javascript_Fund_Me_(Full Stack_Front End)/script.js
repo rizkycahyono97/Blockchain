1b593039ce92e751a04309ae3dc80fbc93a2eb1d
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-02-13 21:42:25 +0700

    FINISH Lesson_8:_HTML_Javascript_Fund_Me_(Full Stack_Front End)

--- FreeCodeCamp/Lesson_8:_HTML_Javascript_Fund_Me_(Full Stack_Front End)/script.js
+++ FreeCodeCamp/Lesson_8:_HTML_Javascript_Fund_Me_(Full Stack_Front End)/script.js
@@ -6,12 +6,24 @@ import { abi, contractAddress } from './constant.js';
 
 const connectButton = document.getElementById('connectButton');
 const fundButton = document.getElementById('fundButton');
+const balance = document.getElementById('getBalance');
+const wd = document.getElementById('withdraw');
+
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+balance.onclick = getBalance;
+wd.onclick = withdraw;
+
+let currentAccount = null;
 
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = await window.ethereum.request({
+      method: 'eth_requestAccounts'
+    });
+
+    currentAccount = account[0];
+
     document.getElementById('connectButton').innerHTML = 'Connected';
   } else {
     document.getElementById('connectButton').innerHTML = 'Cant Connect';
@@ -22,18 +34,19 @@ async function fund() {
   const ethAmount = '5';
 
   if (typeof window.ethereum !== 'undefined') {
+    console.log('fund.....');
     try {
       const provider = new BrowserProvider(window.ethereum);
       const signer = await provider.getSigner();
 
       // LOG
-      const balance = await provider.getBalance(signer.address);
-      console.log('Balance:', ethers.formatEther(balance));
-      console.log('Contract address:', contractAddress);
-      console.log('Signer address:', signer.address);
-      console.log('Signer:', signer.address);
-      const network = await provider.getNetwork();
-      console.log('Chain ID:', network.chainId);
+      //   const balance = await provider.getBalance(signer.address);
+      //   console.log('Balance:', ethers.formatEther(balance));
+      //   console.log('Contract address:', contractAddress);
+      //   console.log('Signer address:', signer.address);
+      //   console.log('Signer:', signer.address);
+      //   const network = await provider.getNetwork();
+      //   console.log('Chain ID:', network.chainId);
 
       const contract = new ethers.Contract(contractAddress, abi, signer);
       const txResponse = await contract.fund({
@@ -41,10 +54,44 @@ async function fund() {
       });
 
       const txReceipt = await txResponse.wait(1);
-      console.log(txReceipt.blockHash);
+      console.log('Blockhash: ', txReceipt.blockHash);
       console.log('Done');
     } catch (error) {
       console.log(error.message);
+    }
+  }
+}
+
+async function getBalance() {
+  if (window.ethereum !== 'undefined') {
+    console.log('getBalance........');
+
+    try {
+      const provider = new BrowserProvider(window.ethereum);
+      const balance = await provider.getBalance(contractAddress);
+
+      console.log(ethers.formatEther(balance), 'ETH');
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+}
+
+// only owner
+async function withdraw() {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('withdraw......');
+
+    try {
+      const provider = new BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      const txResponse = await contract.withdraw();
+      const txReceipt = await txResponse.wait(1);
+      console.log('Blockhash: ', txReceipt.blockHash);
+      console.log('Done');
+    } catch (error) {
+      console.log(error);
     }
   }
 }
