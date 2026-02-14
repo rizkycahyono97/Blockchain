3f4648a66ffd7272299b439d75db21de32e9a3aa
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-02-14 20:43:00 +0700

    add VRFConsumerBaseV2Plus

--- FreeCodeCamp/Lesson_9_Hardhat_Smart_Contract_Lottery/contracts/Raffle.sol
+++ FreeCodeCamp/Lesson_9_Hardhat_Smart_Contract_Lottery/contracts/Raffle.sol
@@ -9,9 +9,12 @@ pragma solidity 0.8.28;
  * 5. chainlink oracle -> automatic executing
  */
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 error Raffle__notEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     // state variabel
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -19,7 +22,10 @@ contract Raffle {
     //event
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vRFConsumerBaseV2Plus,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2Plus(vRFConsumerBaseV2Plus) {
         i_entranceFee = entranceFee;
     }
 
@@ -29,10 +35,21 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] calldata _randomWords
+    ) internal override {}
+
+    /**
+     * @notice getter for i_entranceFee
+     */
     function getEntrance() public view returns (uint256) {
         return i_entranceFee;
     }
 
+    /**
+     * @notice getter for s_players[index]
+     */
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
