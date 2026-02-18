aa4e9f96c1692bcb84db5369586b69053276cf38
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-02-18 21:54:32 +0700

    FIXED BUG unit test

--- FreeCodeCamp/Lesson_9_Hardhat_Smart_Contract_Lottery/test/unit/Raffle.test.ts
+++ FreeCodeCamp/Lesson_9_Hardhat_Smart_Contract_Lottery/test/unit/Raffle.test.ts
@@ -1,3 +1,4 @@
+import { rejects } from 'assert';
 import { expect } from 'chai';
 import { network } from 'hardhat';
 
@@ -6,6 +7,8 @@ const { ethers } = await network.connect();
 describe('Raffle - deployment', function () {
   let raffle: any;
   let deployer: any;
+  let vrfCoordinatorV2_5Mock: any;
+  let subId: any;
 
   const entraceFee = ethers.parseEther('0.1');
   const interval = 60;
@@ -15,7 +18,7 @@ describe('Raffle - deployment', function () {
     '81002506649389566401102351510037004838750721837094787803680786457673035293785';
   const callBackGasLimit = 500000;
   const vrfCoordinator = '0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B';
-  const enableNativePayment = true;
+  const enableNativePayment = false; // HARUS FALSE kalau di local
 
   beforeEach(async function () {
     const signers = await ethers.getSigners();
@@ -25,16 +28,16 @@ describe('Raffle - deployment', function () {
     const VRFCoordinatorV2_5MockFactory = await ethers.getContractFactory(
       'VRFCoordinatorV2_5MockWrapper'
     );
-    const vrfCoordinatorV2_5Mock = await VRFCoordinatorV2_5MockFactory.deploy(
-      ethers.parseEther('0.1'),
-      1e9,
-      4e15
+    vrfCoordinatorV2_5Mock = await VRFCoordinatorV2_5MockFactory.deploy(
+      ethers.parseEther('0.01'),
+      1e9, // gasPriceLink
+      1e15
     );
 
     //create subscription
     const tx = await vrfCoordinatorV2_5Mock.createSubscription();
     const txReceipt = await tx.wait(1);
-    const subId = (txReceipt?.logs[0] as any).args.subId;
+    subId = (txReceipt?.logs[0] as any).args.subId;
     await vrfCoordinatorV2_5Mock.fundSubscription(
       subId,
       ethers.parseEther('100')
@@ -89,9 +92,10 @@ describe('Raffle - deployment', function () {
 
   describe('EnterRaffle', function () {
     it('reverts when not enough ETH is sent', async function () {
-      await expect(
-        raffle.enterRaffle({ value: 0 })
-      ).to.be.revertedWithCustomError(raffle, 'Raffle__notEnoughETHEntered');
+      await expect(raffle.enterRaffle()).to.be.revertedWithCustomError(
+        raffle,
+        'Raffle__notEnoughETHEntered'
+      );
     });
 
     it('records player when they enter', async function () {
@@ -108,16 +112,133 @@ describe('Raffle - deployment', function () {
     });
 
     it('reverts when raffle is not open', async function () {
+      // 1. Masuk raffle agar s_players tidak kosong
+      await raffle.enterRaffle({ value: entraceFee });
+
+      // 2. Manipulasi waktu & trigger upkeep agar status jadi CALCULATING
+      await ethers.provider.send('evm_increaseTime', [interval + 1]);
+      await ethers.provider.send('evm_mine', []);
+      await raffle.performUpkeep('0x');
+
+      // 3. Sekarang status sudah CALCULATING, coba masuk lagi
+      await expect(
+        raffle.enterRaffle({ value: entraceFee })
+      ).to.be.revertedWithCustomError(raffle, 'Raffle__RaffleNotOpen');
+    });
+  });
+
+  describe('CheckUpKeep', function () {
+    it("returns false if people haven't sent any ETH", async function () {
+      await ethers.provider.send('evm_increaseTime', [interval + 1]);
+      await ethers.provider.send('evm_mine');
+
+      const result = await raffle.checkUpkeep('0x');
+      const upkeepNeeded = result[0];
+      expect(upkeepNeeded).to.equal(false);
+    });
+
+    it('returns false if raffle is not open', async function () {
       await raffle.enterRaffle({ value: entraceFee });
 
       await ethers.provider.send('evm_increaseTime', [interval + 1]);
       await ethers.provider.send('evm_mine');
 
-      await raffle.performUpkeep('0x');
+      await raffle.performUpkeep('0x'); // supaya jadi calculating
 
+      const raffleState = await raffle.getRaffleState();
+      const result = await raffle.checkUpkeep('0x');
+      const upkeepNeeded = result[0];
+
+      expect(raffleState).to.equal(1);
+      expect(upkeepNeeded).to.equal(false);
+    });
+
+    it("returns false if enough time hasn't passed", async function () {
+      await raffle.enterRaffle({ value: entraceFee });
+
+      await ethers.provider.send('evm_increaseTime', [interval - 5]);
+      await ethers.provider.send('evm_mine');
+
+      const result = await raffle.checkUpkeep('0x');
+      const upkeepNeeded = result[0];
+      expect(upkeepNeeded).to.equal(false);
+    });
+
+    it('returns true if enough time has passed, has players, has balance, and is open', async function () {
+      await raffle.enterRaffle({ value: entraceFee });
+
+      await ethers.provider.send('evm_increaseTime', [interval + 1]);
+      await ethers.provider.send('evm_mine');
+
+      const result = await raffle.checkUpkeep('0x');
+      const upkeepNeeded = result[0];
+      expect(upkeepNeeded).to.equal(true);
+    });
+  });
+
+  describe('fulfillRandomWords', function () {
+    it('can only be called after performUpkeep', async function () {
       await expect(
-        raffle.enterRaffle({ value: entraceFee })
-      ).to.revertedWithCustomError(raffle, 'Raffle__RaffleNotOpen');
+        vrfCoordinatorV2_5Mock.fulfillRandomWords(0, raffle.target)
+      ).to.be.revert(ethers);
+    });
+
+    it('picks a winner, resets the lottery, and sends money', async function () {
+      const additionalEntrants = 3;
+      const startingAccountIndex = 1;
+      const account = await ethers.getSigners();
+
+      //memasukan beberapa player
+      for (
+        let i = startingAccountIndex;
+        i < startingAccountIndex + additionalEntrants;
+        i++
+      ) {
+        const raffleConnected = raffle.connect(account[i]);
+        await raffleConnected.enterRaffle({ value: entraceFee });
+      }
+
+      const startingTimeStamps = await raffle.getLastestTimeStamp();
+
+      await new Promise<void>(async (resolve, rejects) => {
+        raffle.once('PickedWinner', async () => {
+          console.log('Event PickedWinner');
+          try {
+            const recentWinner = await raffle.getRecentWinner();
+            const raffleState = await raffle.getRaffleState();
+            const endingTimeStamp = await raffle.getLastestTimeStamp();
+            const numPlayer = await raffle.getNumberOfPlayer();
+            const winnerEndingBalance =
+              await ethers.provider.getBalance(recentWinner);
+
+            expect(numPlayer).to.equal(0);
+            expect(raffleState).to.equal(0);
+            expect(endingTimeStamp).to.be.gt(startingTimeStamps);
+            expect(winnerEndingBalance).to.be.gt(winnerStartingBalance);
+
+            resolve();
+          } catch (e) {
+            rejects(e);
+          }
+        });
+
+        await ethers.provider.send('evm_increaseTime', [interval + 1]);
+        await ethers.provider.send('evm_mine');
+
+        const tx = await raffle.performUpkeep('0x');
+        const txReceipt = await tx.wait(1);
+
+        const requestId = txReceipt!.logs[1].args.requestId;
+
+        const winnerStartingBalance = await ethers.provider.getBalance(
+          account[1].address
+        );
+
+        await vrfCoordinatorV2_5Mock.fulfillRandomWords(
+          requestId,
+          raffle.target
+        );
+      });
     });
   });
 });
