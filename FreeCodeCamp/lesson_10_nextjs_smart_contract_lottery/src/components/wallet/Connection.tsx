485b457f383b3659d78af1bc0ba2b2970d63e9a0
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-02-22 23:50:33 +0700

    add metamask SDK

--- FreeCodeCamp/lesson_10_nextjs_smart_contract_lottery/src/components/wallet/Connection.tsx
+++ FreeCodeCamp/lesson_10_nextjs_smart_contract_lottery/src/components/wallet/Connection.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import { useConnection, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi';
+import { Button } from '@/components/ui/button';
 
 export function Connection() {
   const { address } = useConnection();
@@ -8,9 +10,13 @@ export function Connection() {
 
   return (
     <div>
-      {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
+      {ensAvatar && (
+        <Image alt="ENS Avatar" src={ensAvatar} width={40} height={40} />
+      )}
       {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
-      <button onClick={() => disconnect()}>Disconnect</button>
+      <Button className="mt-5" onClick={() => disconnect()}>
+        Disconnect
+      </Button>
     </div>
   );
 }
