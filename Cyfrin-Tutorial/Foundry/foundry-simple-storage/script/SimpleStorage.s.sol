8597307aba5e1bdf2b74053293992464c2857954
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-03-13 22:32:18 +0700

    modified file

--- Cyfrin-Tutorial/Foundry/foundry-simple-storage/script/SimpleStorage.s.sol
+++ Cyfrin-Tutorial/Foundry/foundry-simple-storage/script/SimpleStorage.s.sol
@@ -8,7 +8,7 @@ contract DeployScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        SimpleStorage simpleStorage = new SimpleStorage();
+        new SimpleStorage();
 
         vm.stopBroadcast();
     }
